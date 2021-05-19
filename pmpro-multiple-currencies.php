9df11c3009a04c06b0ec97b6798829c674953b95
Louis <louiswol94@gmail.com>
2021-05-19 22:04:58 +0200

    initial commit

---
+++ pmpro-multiple-currencies.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Plugin Name:       Multiple Currencies for Paid Memberships Pro
+ * Description:       Change currencies depending on Paid Memberships Pro level.
+ * Version:           0.0.1
+ * Author:            Louis Wolmarans
+ * Author URI:        https://letmefreelance.co.za/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ // Add a currency setting on the level edit screen
+function lmf_action_pmpro_membership_level_after_other_settings() {
+
+	global $pmpro_currencies;
+
+    if ( isset( $_REQUEST['edit'] ) && '-1' !== $_REQUEST['edit'] ) {
+        $edit = intval( $_REQUEST['edit'] );
+        $selected_custom_currency = get_pmpro_membership_level_meta( $edit, 'pmpro_custom_currency', true );
+    } else {
+        $selected_custom_currency = "DEFAULT";
+    }
+    ?>
+
+    <h2 class="title"><?php esc_html_e( 'Currency Settings', 'paid-memberships-pro' ); ?></h2>
+
+    <table class="form-table">
+        <tbody>
+            <tr>
+                <th scope="row" valign="top">
+                    <label><?php _e('Choose a currency for this membership level', 'paid-memberships-pro' );?>:</label>
+                </th>
+                <td>
+                    <select name="pmpro_custom_currency">
+                        <option value="DEFAULT" <?php if ( $selected_custom_currency == "DEFAULT" ) { ?>selected="selected"<?php } ?>>Use the default currency</option>
+                        <?php
+                            foreach ( $pmpro_currencies as $key => $val) {
+                                if ( is_array( $val ) ) {
+                                    $cdescription = $val['name'];
+                                    $custom_symbol = $val['symbol'];
+                                } else {
+                                    $cdescription = $val;
+                                    $custom_symbol = $key;
+                                    if ( strpos( $cdescription, '(' ) !== false && strpos( $cdescription, ')' ) !== false ) {
+                                        preg_match('#\((.*?)\)#', $cdescription, $match);
+                                        $custom_symbol = $match[1];
+                                    }
+                                }
+                                $selected = "";
+                                if ( $selected_custom_currency !== "DEFAULT" ) {
+                                    $c_arr = explode( ",", $selected_custom_currency );
+                                    if ( $c_arr[0] == $key ) {
+                                        $selected = "selected";
+                                    }
+                                }
+
+                                $option_value = $key.",".$custom_symbol;
+                            ?>
+                            <option value="<?php echo esc_attr( $option_value ); ?>" <?php echo $selected; ?>><?php echo $cdescription; ?></option>
+                            <?php
+                            }
+                        ?>
+                    </select>
+                    <p class="description">Make sure your payment gateway supports the selected currency.</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <?php
+
+}
+add_action( "pmpro_membership_level_after_other_settings", "lmf_action_pmpro_membership_level_after_other_settings", 10, 0 );
+
+// save the custom currency setting in level meta
+function lmf_pmpro_save_membership_level( $level_id ) {
+
+    if ( $level_id <= 0 ) return;
+
+	$pmpro_custom_currency = sanitize_text_field( $_REQUEST['pmpro_custom_currency'] );
+	update_pmpro_membership_level_meta( $level_id, 'pmpro_custom_currency', $pmpro_custom_currency );
+
+}
+add_action( "pmpro_save_membership_level", "lmf_pmpro_save_membership_level" );
+
+// main function to check for a currency level and update currencies
+function lmf_update_currency_per_level( $level_id ) {
+
+	global $pmpro_currency, $pmpro_currency_symbol, $level_currencies;
+
+    $selected_custom_currency = get_pmpro_membership_level_meta( $level_id, 'pmpro_custom_currency', true );
+
+    if (
+        $selected_custom_currency !== false && 
+        ! empty ( $selected_custom_currency ) && 
+        $selected_custom_currency !== "DEFAULT" 
+    ) {
+        $level_currency = explode( ",", $selected_custom_currency );
+        $pmpro_currency = $level_currency[0];
+        if ( empty ( $level_currency[1] ) ) {
+            $pmpro_currency_symbol = $level_currency[0];
+        } else {
+            $pmpro_currency_symbol = $level_currency[1];
+        }
+    }
+
+}
+
+// change currency on checkout page
+function lmf_pmpro_checkout_level( $level ) {
+
+	lmf_update_currency_per_level( $level->id );
+	return $level;
+
+}
+add_filter( "pmpro_checkout_level", "lmf_pmpro_checkout_level" );
+
+// change currency when sent as a request param
+function lmf_init_currency_check() {
+
+	if ( ! empty( $_REQUEST['level'] ) )
+		return lmf_update_currency_per_level( intval( $_REQUEST['level'] ) );
+
+}
+add_action( "init", "lmf_init_currency_check" );
+
+// params in the admin
+function lmf_admin_init_currency_check() {
+
+	if (
+        ! empty( $_REQUEST['edit'] ) &&
+        ! empty( $_REQUEST['page'] ) &&
+        'pmpro-membershiplevels' == $_REQUEST['page'] &&
+        '-1' !== $_REQUEST['edit']
+    )
+	
+    return lmf_update_currency_per_level( intval( $_REQUEST['edit'] ) );
+
+}
+add_action( "admin_init", "lmf_admin_init_currency_check" );
+
+function lmf_pmpro_level_cost_text( $r, $level, $tags, $short ) {
+
+    global $pmpro_currency, $pmpro_currency_symbol, $pmpro_currencies;
+    $selected_custom_currency = get_pmpro_membership_level_meta( $level->id, 'pmpro_custom_currency', true );
+
+    if ( 
+        $selected_custom_currency !== false && 
+        ! empty ( $selected_custom_currency ) && 
+        $selected_custom_currency !== "DEFAULT" 
+    ) {
+        $level_currency = explode( ",", $selected_custom_currency );
+        $curr = empty( $level_currency[1] ) ? $level_currency[0] : $level_currency[1];
+        return str_replace($pmpro_currency_symbol, $curr, $r);
+    }
+    
+    return $r;
+	
+}
+add_filter( "pmpro_level_cost_text", "lmf_pmpro_level_cost_text", 10, 4 );
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Multiple Currencies for Paid Memberships Pro ===
+Contributors: louiswol94
+Donate link: https://letmefreelance.co.za/donate
+Tags: pmpro, currency
+Requires at least: 4.7
+Tested up to: 5.7.1
+Stable tag: 0.0.1
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Add multiple currencies to Paid Memberships Pro.
+
+== Description ==
+
+This plugin allows you to add multiple currencies to the Paid Memberships Pro plugin. This will enable you to specify a different currency per membership level, or it will just use the default currency setting if left empty.
+
+Please note: this will update the currency on the following pages:
+* Membership Levels page
+* Checkout page
+* Admin membership Levels page
+* Admin Level settings page
+
+Due to no hooks and filters on the following sections of PMPro's core, the following pages will not be able to reflect the custom currencies at the moment:
+* Admin Order & Reports page
+* Customer Account & Invoice history
+* Emails
+
+Please note: this is not an official add on from the PMPro team.
+
+Credit: Plugin was inspired by this gist https://gist.github.com/strangerstudios/8806443
+
+== Screenshots ==
+
+1. Level setting
+2. Level setting options
+3. Admin Membership Levels page
+4. Customer Membership Levels page
+5. Checkout page
+
+== Changelog ==
+
+= 0.0.1 =
+Inital release.
